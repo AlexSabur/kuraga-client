35fa8331756c131945a7d6d0c8bb46ee79596c36
AlexSabur <alexsabur@live.ru>
2020-01-18 03:44:11 +0300

    upd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,10 @@
 module.exports = {
   apps: [
-    // {
-    //   name: "API",
-    //   script: "app.js",
-
-    //   // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    //   args: "one two",
-    //   instances: 1,
-    //   autorestart: true,
-    //   watch: false,
-    //   max_memory_restart: "1G",
-    //   env: {
-    //     NODE_ENV: "development"
-    //   },
-    //   env_production: {
-    //     NODE_ENV: "production"
-    //   }
-    // }
     {
       name: "client",
       port: 80,
       script: "./node_modules/nuxt/bin/nuxt-start",
-      cwd: "/home/kuraga-user/kuraga-client",
+      // cwd: "/home/kuraga-user/kuraga-client",
       env: {
         NODE_ENV: "development"
       },
